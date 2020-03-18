27ab1e3af6ba90f097083a11df632a0b261b3043
tupseny <marsik98@gmail.com>
2020-03-18 22:46:55 +0300

    New migrations. To create db tables for meals

--- database/migrations/2020_03_18_143707_create_menu_tables.php
+++ database/migrations/2020_03_18_143707_create_menu_tables.php
@@ -6,6 +6,11 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateMenuTables extends Migration
 {
+    const MEAL_KEY = 'meals';
+    const MENU_GROUP_KEY = 'menu_groups';
+    const INGREDIENT_KEY = 'ingredients';
+    const MEAL_INGREDIENT_KEY = 'meal_ingredient';
+
     /**
      * Run the migrations.
      *
@@ -26,10 +31,10 @@ class CreateMenuTables extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('meal_ingredient');
-        Schema::dropIfExists('ingredient');
-        Schema::dropIfExists('meal');
-        Schema::dropIfExists('menu_group');
+        Schema::dropIfExists(self::MEAL_INGREDIENT_KEY);
+        Schema::dropIfExists(self::INGREDIENT_KEY);
+        Schema::dropIfExists(self::MEAL_KEY);
+        Schema::dropIfExists(self::MENU_GROUP_KEY);
     }
 
     /**
@@ -40,11 +45,23 @@ class CreateMenuTables extends Migration
      *  create ONLY after:
      *  MEAL, INGREDIENT
      */
-    private function createMealIngredient(){
-        Schema::create('meal_ingredient', function (Blueprint $t){
-            $t->bigInteger('meal_id');
-            $t->bigInteger('ingredient_id');
+    private function createMealIngredient()
+    {
+        Schema::create(self::MEAL_INGREDIENT_KEY, function (Blueprint $t) {
+            $t->unsignedBigInteger('meal_id');
+            $t->unsignedBigInteger('ingredient_id')->nullable();
             $t->unsignedFloat('amount');
+
+            $t->primary(['meal_id', 'ingredient_id']);
+            $t->foreign('meal_id')
+                ->references('id')
+                ->on(self::MEAL_KEY)
+                ->onDelete('cascade');
+
+            $t->foreign('ingredient_id')
+                ->references('id')
+                ->on(self::INGREDIENT_KEY)
+                ->onDelete('set null');
         });
     }
 
@@ -53,9 +70,10 @@ class CreateMenuTables extends Migration
      *
      *  Contains ingredients
      */
-    private function createIngredients(){
-        Schema::create('ingredient', function (Blueprint $t){
-            $t->id();
+    private function createIngredients()
+    {
+        Schema::create(self::INGREDIENT_KEY, function (Blueprint $t) {
+            $t->id()->primary();
             $t->string('name');
             $t->string('unit');
         });
@@ -66,9 +84,10 @@ class CreateMenuTables extends Migration
      *
      *  Contains menu categories (pizzas, drinks, ...)
      */
-    private function createMenuGroup(){
-        Schema::create('menu_group', function (Blueprint $t){
-            $t->id();
+    private function createMenuGroup()
+    {
+        Schema::create(self::MENU_GROUP_KEY, function (Blueprint $t) {
+            $t->id()->primary();
             $t->string('name');
         });
     }
@@ -81,12 +100,18 @@ class CreateMenuTables extends Migration
      *  Create ONLY after:
      *  MENU_GROUP
      */
-    private function createMeal(){
-        Schema::create('meal', function (Blueprint $t) {
-            $t->id();
+    private function createMeal()
+    {
+        Schema::create(self::MEAL_KEY, function (Blueprint $t) {
+            $t->id()->primary();
             $t->string('name');
-            $t->bigInteger('group_id');
+            $t->unsignedBigInteger('group_id')->index();
             $t->float('price');
+
+            $t->foreign('group_id')
+                ->references('id')
+                ->on(self::MENU_GROUP_KEY)
+                ->onDelete('restrict');
         });
     }
 }
