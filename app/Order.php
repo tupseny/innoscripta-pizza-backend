7a7493f1d5b3dda0e7e3f162aa1e8ca8b770abb3
msikora <marsik98@gmail.com>
2020-03-30 04:01:10 +0300

    Order model

---
+++ app/Order.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = ['user_id'];
+
+    public function meals(){
+        return $this->belongsToMany(Meal::class, 'order_meal');
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
