a660cd006f704db63cf4697c3f889b693d176dc7
Martin <marsik98@gmail.com>
2020-04-01 18:21:13 +0300

    Not random seeder

--- database/seeds/MealMenuSeeder.php
+++ database/seeds/MealMenuSeeder.php
@@ -13,11 +13,41 @@ class MealMenuSeeder extends Seeder
      */
     public function run()
     {
-        factory(MenuGroup::class, rand(2, 5))->create()->each(function (MenuGroup $group){
-           $group->meals()->createMany(
-               factory(Meal::class, rand(1, 5))->make()->toArray()
-           );
-        });
+        $pizza = new MenuGroup([
+            'name' => 'Pizza',
+        ]);
+        $pizza->save();
+
+        $drink = new MenuGroup([
+            'name' => 'Drink',
+        ]);
+        $drink->save();
+
+        $snacks = new MenuGroup([
+            'name' => 'Snacks',
+        ]);
+        $snacks->save();
+
+        $pizza->meals()->create([
+            new Meal(['name' => 'Farm']),
+            new Meal(['name' => 'Margarita']),
+            new Meal(['name' => 'Meat']),
+            new Meal(['name' => 'Olive']),
+            new Meal(['name' => 'Onion']),
+            new Meal(['name' => 'Sea']),
+            new Meal(['name' => 'Solyami'])
+        ]);
+
+        $drink->meals()->create([
+            new Meal(['name' => 'Cola']),
+            new Meal(['name' => 'Fanta']),
+            new Meal(['name' => 'Pepsi']),
+            new Meal(['name' => 'Sprite']),
+        ]);
+
+        $snacks->meals()->create([
+            new Meal(['name' => 'Chips']),
+        ]);
 
 //        Empty group
         factory(MenuGroup::class, rand(1,3))->create();
